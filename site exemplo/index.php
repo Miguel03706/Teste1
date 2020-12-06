51a8b8cd5e616e3edbe4c66886891b1da950b13f
Miguel <75583778+Miguel03706@users.noreply.github.com>
2020-12-06 17:09:25 -0300

    Create index.php

---
+++ site exemplo/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro site em repositório</title>
+</head>
+<body>
+    <h1>Olá mundo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deleniti autem delectus, est aliquid temporibus magnam animi sequi quod recusandae molestiae odio maxime quaerat ducimus saepe odit. Vitae, ex eaque.</p>
+</body>
+</html>
