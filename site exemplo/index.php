c19fb994081892a7e516bd3d7a9893f4e9cbd6ac
Miguel <75583778+Miguel03706@users.noreply.github.com>
2020-12-06 17:11:11 -0300

    adicionei paragrafo

--- site exemplo/index.php
+++ site exemplo/index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Olá mundo</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deleniti autem delectus, est aliquid temporibus magnam animi sequi quod recusandae molestiae odio maxime quaerat ducimus saepe odit. Vitae, ex eaque.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates nulla, itaque assumenda accusamus omnis, fugiat atque at dolor non delectus ullam pariatur quidem dolorem asperiores exercitationem quia? Saepe, autem repudiandae.</p>
 </body>
 </html>
